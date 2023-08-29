b43b298c455f08cf207500e1da5927f839911d16
DESKTOP-RAJBO8B\bilel-dev <bilel.daikhi@gmail.com>
2023-08-29 11:24:32 +0100

    ignore dev files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
   baseUrl : `${window.location.protocol}//${window.location.hostname}/portfolio/`,
+  extravar:'',
   fireBaseApi:'https://bilel-daikhi-portfolio-default-rtdb.europe-west1.firebasedatabase.app',
 };
